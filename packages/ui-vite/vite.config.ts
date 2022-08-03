922d7bd98b2723984aa133d5b70f24b697e88d22
W.J. “dysbulic” H <dys@dhappy.org>
2022-08-03 10:51:21 -0400

    downgradin' Chakra UI to get a `dist` folder 📮

--- packages/ui-vite/vite.config.ts
+++ packages/ui-vite/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsConfigPaths from "rollup-plugin-tsconfig-paths"
+import nodeResolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    tsConfigPaths(),
+    react(),
+  ]
 })
