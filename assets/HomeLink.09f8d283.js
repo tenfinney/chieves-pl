0bb618f605757742a1aabb92d21ef4273ccee700
tenfinney <scott@1box.onmicrosoft.com>
2022-09-03 11:08:16 -0700

    Updatin’ GitHub Pages

---
+++ assets/HomeLink.09f8d283.js
@@ -0,0 +1,15 @@
+import { b as jsx } from "./index.2ada5cbf.js";
+import { f as LinkedSVG } from "./index.30f75012.js";
+const HomeLink = () => /* @__PURE__ */ jsx(LinkedSVG, {
+  href: "/",
+  svg: "../favicon.svg",
+  h: "3rem",
+  w: "2rem",
+  position: "fixed",
+  left: "0.25rem",
+  top: "0.25rem"
+});
+export {
+  HomeLink as H
+};
+//# sourceMappingURL=HomeLink.09f8d283.js.map
