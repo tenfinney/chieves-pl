a8ff7f4c3780c1fb4692b29f3149fabb462edad4
Scott Stevenson <scott@1box.onmicrosoft.com>
2022-09-23 00:11:38 -0700

    Updatin’ GitHub Pages

---
+++ assets/edit.b407e588.js
@@ -0,0 +1,77 @@
+import { aZ as useParams, r as react, x as useWeb3, j as jsxs, q as Box, b as jsx, z as HelmetExport } from "./index.a0657232.js";
+import { p as deregexify, r as regexify, h as httpURL, l as lib, e as extractMessage } from "./index.f743cbe2.js";
+import { O as OptionsForm } from "./MaxForm.22048063.js";
+import { H as HomeLink, A as Alert, a as AlertIcon, b as AlertTitle, c as AlertDescription } from "./HomeLink.019b123a.js";
+import "./chakra-ui-icons.esm.c1c91dac.js";
+import "./chakra-ui-textarea.esm.f3b663dc.js";
+import "./SubmitButton.40688c93.js";
+import "./chakra-ui-toast.esm.bd87fdbc.js";
+const Edit = () => {
+  const {
+    nftId
+  } = useParams();
+  const tokenId = react.exports.useMemo(() => deregexify(nftId), [nftId]);
+  const [metadata, setMetadata] = react.exports.useState();
+  const [metaURI, setMetaURI] = react.exports.useState();
+  const [error, setError] = react.exports.useState();
+  const {
+    roContract
+  } = useWeb3();
+  react.exports.useEffect(() => {
+    const getMetadata = async () => {
+      if (roContract && tokenId) {
+        try {
+          const metaURI2 = await roContract.uri(tokenId);
+          const url = httpURL(metaURI2);
+          if (!metaURI2 || metaURI2 === "") {
+            setMetadata(null);
+          } else {
+            const response = await fetch(url);
+            const body = await response.text();
+            try {
+              setMetaURI(metaURI2);
+              setMetadata(lib.parse(body));
+            } catch (error2) {
+              console.error({
+                url,
+                tokenId,
+                metaURI: metaURI2,
+                error: error2,
+                body
+              });
+            }
+          }
+        } catch (err) {
+          setError(extractMessage(err));
+        }
+      }
+    };
+    getMetadata();
+  }, [roContract, tokenId]);
+  return /* @__PURE__ */ jsxs(Box, {
+    ml: 16,
+    bg: "black",
+    children: [/* @__PURE__ */ jsx(HelmetExport, {
+      children: /* @__PURE__ */ jsxs("title", {
+        children: ["Claim Token: Edit #", tokenId && regexify(tokenId)]
+      })
+    }), /* @__PURE__ */ jsx(HomeLink, {}), error && /* @__PURE__ */ jsxs(Alert, {
+      status: "error",
+      children: [/* @__PURE__ */ jsx(AlertIcon, {}), /* @__PURE__ */ jsx(AlertTitle, {
+        children: "`setMetadata` Error"
+      }), /* @__PURE__ */ jsx(AlertDescription, {
+        children: error
+      })]
+    }), /* @__PURE__ */ jsx(OptionsForm, {
+      purpose: "update",
+      tokenId,
+      metadata,
+      metaURI
+    })]
+  });
+};
+export {
+  Edit,
+  Edit as default
+};
+//# sourceMappingURL=edit.b407e588.js.map
