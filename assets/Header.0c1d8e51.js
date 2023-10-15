33bd9842f111288f1e02e6acc46c53963ce18f2d
tenfinney <scott@1box.onmicrosoft.com>
2023-10-15 10:51:51 -0700

    Updatin’ GitHub Pages

---
+++ assets/Header.0c1d8e51.js
@@ -0,0 +1,43 @@
+import { d as jsxs, F as Flex, j as jsx } from "./index.9ba8fd68.js";
+import { a as Tooltip, b as LinkedSVG } from "./index.e520b641.js";
+const Header = ({
+  links = {
+    cup: "/new",
+    sign: "/"
+  },
+  ...props
+}) => /* @__PURE__ */ jsxs(Flex, {
+  grow: 1,
+  ...props,
+  children: [/* @__PURE__ */ jsx(Tooltip, {
+    hasArrow: true,
+    label: "Create A New Token Type",
+    children: /* @__PURE__ */ jsx(LinkedSVG, {
+      ml: "120px",
+      mb: "120px",
+      w: "75%",
+      h: "auto",
+      svg: "Circles.svg",
+      href: links.cup,
+      title: "Create a new Token",
+      id: "createatoken"
+    })
+  }), /* @__PURE__ */ jsx(Tooltip, {
+    hasArrow: true,
+    label: "List Existing Tokens",
+    children: /* @__PURE__ */ jsx(LinkedSVG, {
+      ml: "220px",
+      mb: "220px",
+      w: "75%",
+      h: "auto",
+      svg: "Circles2.svg",
+      href: links.sign,
+      title: "View Existing Tokens",
+      id: "createatoken"
+    })
+  })]
+});
+export {
+  Header as H
+};
+//# sourceMappingURL=Header.0c1d8e51.js.map
