7fca80b260ed90f549eda6a49a96a8f3c2d47cf2
tenfinney <scott@onelaw.us>
2024-01-29 08:15:29 -0800

    Updatin’ GitHub Pages

---
+++ assets/self-mint.f6f9cc18.js
@@ -0,0 +1,76 @@
+import { p as useWeb3, r as react, d as jsxs, C as Container, s as HelmetExport, j as jsx, S as Stack, aX as useParams } from "./index.d0605c9d.js";
+import { e as extractMessage, r as regexify, p as deregexify } from "./index.55a1ed27.js";
+import { View } from "./view.c1ff6e23.js";
+import { S as SubmitButton } from "./SubmitButton.799612bc.js";
+import { u as useToast } from "./chakra-ui-toast.esm.97788d9b.js";
+import "./HomeLink.746bdf5a.js";
+const SelfMint = ({
+  tokenId
+}) => {
+  const {
+    rwContract,
+    address
+  } = useWeb3();
+  const [processing, setProcessing] = react.exports.useState(false);
+  const toast = useToast();
+  const mint = react.exports.useCallback(async () => {
+    try {
+      setProcessing(true);
+      const tx = await rwContract["mint(address[],uint256,bytes)"]([address], BigInt(tokenId), []);
+      await tx.wait();
+    } catch (error) {
+      console.error({
+        error
+      });
+      toast({
+        title: "Minting Error",
+        description: extractMessage(error),
+        status: "error",
+        isClosable: true,
+        duration: 1e4
+      });
+    } finally {
+      setProcessing(false);
+    }
+  }, [address, rwContract, toast, tokenId]);
+  return /* @__PURE__ */ jsxs(Container, {
+    maxW: "40rem",
+    my: 10,
+    children: [/* @__PURE__ */ jsxs(HelmetExport, {
+      children: [/* @__PURE__ */ jsxs("title", {
+        children: ["Self-Mint Token or Digital Asset #", regexify(tokenId)]
+      }), /* @__PURE__ */ jsx("meta", {
+        name: "description",
+        content: "Mint a Digital Asset or Token"
+      })]
+    }), /* @__PURE__ */ jsxs(Stack, {
+      as: "form",
+      onSubmit: mint,
+      children: [/* @__PURE__ */ jsx(SubmitButton, {
+        purpose: "mint",
+        processing
+      }), /* @__PURE__ */ jsx(View, {
+        tokenId,
+        header: false
+      }), /* @__PURE__ */ jsx(SubmitButton, {
+        purpose: "mint",
+        processing
+      })]
+    })]
+  });
+};
+const SelfMintPage = () => {
+  const {
+    nftId
+  } = useParams();
+  const tokenId = deregexify(Array.isArray(nftId) ? nftId[0] : nftId);
+  return /* @__PURE__ */ jsx(SelfMint, {
+    tokenId
+  });
+};
+export {
+  SelfMint,
+  SelfMintPage,
+  SelfMintPage as default
+};
+//# sourceMappingURL=self-mint.f6f9cc18.js.map
